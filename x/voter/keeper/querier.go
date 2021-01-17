e627f19e5deee12a177b33d88257f34fed269c09
Victor Yeo <victoryeo1974@gmail.com>
2021-01-17 21:04:32 +0800

    add vote type

--- x/voter/keeper/querier.go
+++ x/voter/keeper/querier.go
@@ -4,6 +4,8 @@ import (
   // this line is used by starport scaffolding # 1
 	"github.com/victoryeo/voter/x/voter/types"
 		
+	
+		
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -15,6 +17,10 @@ func NewQuerier(k Keeper) sdk.Querier {
 	return func(ctx sdk.Context, path []string, req abci.RequestQuery) ([]byte, error) {
 		switch path[0] {
     // this line is used by starport scaffolding # 2
+		case types.QueryListVote:
+			return listVote(ctx, k)
+		case types.QueryGetVote:
+			return getVote(ctx, path[1:], k)
 		case types.QueryListPoll:
 			return listPoll(ctx, k)
 		case types.QueryGetPoll:
