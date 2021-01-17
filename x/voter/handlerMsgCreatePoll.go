4e479502401797b82d0f340924fcbcf84f061c38
Victor Yeo <victoryeo1974@gmail.com>
2021-01-17 21:09:10 +0800

    Introducing a fee for creating polls

--- x/voter/handlerMsgCreatePoll.go
+++ x/voter/handlerMsgCreatePoll.go
@@ -5,9 +5,20 @@ import (
 
 	"github.com/victoryeo/voter/x/voter/types"
 	"github.com/victoryeo/voter/x/voter/keeper"
+	"github.com/tendermint/tendermint/crypto"
 )
 
 func handleMsgCreatePoll(ctx sdk.Context, k keeper.Keeper, msg types.MsgCreatePoll) (*sdk.Result, error) {
+	var poll = types.Poll{
+		Creator: msg.Creator,
+		Title:   msg.Title,
+		Options: msg.Options,
+	}
+	moduleAcct := sdk.AccAddress(crypto.AddressHash([]byte(types.ModuleName)))
+	payment, _ := sdk.ParseCoins("200token")
+	if err := k.CoinKeeper.SendCoins(ctx, poll.Creator, moduleAcct, payment); err != nil {
+		return nil, err
+	}	
 	k.CreatePoll(ctx, msg)
 
 	return &sdk.Result{Events: ctx.EventManager().Events()}, nil
