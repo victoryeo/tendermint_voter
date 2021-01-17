5f26d88b4f9126acbab846475298cd0d34e7e7e6
Victor Yeo <victoryeo1974@gmail.com>
2021-01-17 21:07:08 +0800

    add vote type

---
+++ x/voter/handlerMsgCreateVote.go
@@ -0,0 +1,14 @@
+package voter
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/victoryeo/voter/x/voter/types"
+	"github.com/victoryeo/voter/x/voter/keeper"
+)
+
+func handleMsgCreateVote(ctx sdk.Context, k keeper.Keeper, msg types.MsgCreateVote) (*sdk.Result, error) {
+	k.CreateVote(ctx, msg)
+
+	return &sdk.Result{Events: ctx.EventManager().Events()}, nil
+}
